6c684da47e3a5b96ebe822fef3a7be541c67d5c8
Abungu Brian <brianabungu@gmail.com>
2023-06-04 08:49:53 +0300

    State Machine Enumerations

---
+++ src/main/java/guru/springframework/msscssm/domain/PaymentEvent.java
@@ -0,0 +1,6 @@
+package guru.springframework.msscssm.domain;
+
+public enum PaymentEvent {
+    AUTHORIZE, AUTH_APPROVED, AUTH_DECLINED, PRE_AUTHORIZE, PRE_AUTH_APPROVED, PRE_AUTH_DECLINED
+
+}
---
+++ src/main/java/guru/springframework/msscssm/domain/PaymentState.java
@@ -0,0 +1,6 @@
+package guru.springframework.msscssm.domain;
+
+public enum PaymentState {
+    AUTH, AUTH_ERROR, NEW, PRE_AUTH, PRE_AUTH_ERROR
+
+}
